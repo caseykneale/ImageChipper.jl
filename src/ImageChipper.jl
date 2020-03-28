12020d6af724f439b5cbae05f07cd1c039210948
casey kneale <ckneale@gmail.com>
2020-03-28 18:26:48 -0400

    added unit tests, and fixed bugs?

--- src/ImageChipper.jl
+++ src/ImageChipper.jl
@@ -1,19 +1,19 @@
 module ImageChipper
-    using ObjectDetectionStats, Images, FileIO, ImageMagick
+    using ObjectDetectionStats#, Images, FileIO, ImageMagick
 
     function chip(  img::Matrix,
                     chip_size::Tuple{Int, Int}      = ( 128, 128 ),
                     overlap::Tuple{Float64,Float64} = ( 0.0, 0.0 ) )
         width, height   = size( img )
-        chips           = Int.( floor.( [ width, height ] ./ chip_size ) ) .+ 1
+        chips           = Int.( ceil.( [ width, height ] ./ chip_size ) ) .+ 1
         overlap_factor  = chip_size
         if overlap != ( 0.0, 0.0 )
-            chips = Int.( round.( ( chips .- 1.0 ) .* ( 1.0 ./ overlap ) ) )  #.+ 1
+            chips = Int.( ceil.( ( chips .- 1.0 ) .* ( 1.0 ./ overlap ) ) )  #.+ 1
             overlap_factor = [  floor( chip_size[ 1 ] * ( overlap[ 1 ] ) ),
                                 floor( chip_size[ 2 ] * ( overlap[ 2 ] ) ) ] .|> Int
             @assert( !any( overlap_factor .≈ chip_size ), "Requested overlap for at least one dimension is infinite. Please choose a smaller value.")
         end
-        chip_view = Array{ eltype(pic),3 }( undef, ( chip_size[ 1 ], chip_size[ 2 ], prod( chips ) ) )
+        chip_view = Array{ eltype(img),3 }( undef, ( chip_size[ 1 ], chip_size[ 2 ], prod( chips ) ) )
         chip_count = 1
         for col in 1 : chips[1], row in 1 : chips[ 2 ]
             x = ( row - 1 ) * overlap_factor[ 1 ]
@@ -36,15 +36,15 @@ module ImageChipper
                                 overlap::Tuple{Float64,Float64} = ( 0.0, 0.0 ),
                                 IoU_threshold::Float64          = 0.0 )
         width, height   = size( img )
-        chips           = Int.( floor.( [ width, height ] ./ chip_size ) ) .+ 1
+        chips           = Int.( ceil.( [ width, height ] ./ chip_size ) ) #.+ 1
         overlap_factor  = chip_size
         if overlap != ( 0.0, 0.0 )
-            chips = Int.( round.( ( chips .- 1.0 ) .* ( 1.0 ./ overlap ) ) )  #.+ 1
+            chips = Int.( ceil.( ( chips .- 1.0 ) .* ( 1.0 ./ overlap ) ) )  #.+ 1
             overlap_factor = [  floor( chip_size[ 1 ] * ( overlap[ 1 ] ) ),
                                 floor( chip_size[ 2 ] * ( overlap[ 2 ] ) ) ] .|> Int
             @assert( !any( overlap_factor .≈ chip_size ), "Requested overlap for at least one dimension is infinite. Please choose a smaller value.")
         end
-        chip_view = Array{ eltype(pic),3 }( undef, ( chip_size[ 1 ], chip_size[ 2 ], prod( chips ) ) )
+        chip_view = Array{ eltype(img),3 }( undef, ( chip_size[ 1 ], chip_size[ 2 ], prod( chips ) ) )
         chip_count = 1
         boxes_in_chip = Dict()
 
@@ -57,8 +57,8 @@ module ImageChipper
             Y = (col == chips[1]) ? ( ( height - chip_size[ 2 ] + 1 ) : height ) : Y
             X = (row == chips[2]) ? ( (  width - chip_size[ 1 ] + 1 ) : width  ) : X
             chip_view[:,:,chip_count] = view( img, X, Y )
-            chip_box = ObjectDetectionStats.box( first(X), first(Y), last(X), last(Y) )
-            boxes_in_chip[ chip_count ] = [ i for (i, box) in enumerte( boxes )
+            chip_box = ObjectDetectionStats.Box( first(X), first(Y), last(X), last(Y) )
+            boxes_in_chip[ chip_count ] = [ i for (i, box) in enumerate( boxes )
                                             if intersection_over_union(chip_box, box) > IoU_threshold ]
 
             chip_count += 1
@@ -69,16 +69,3 @@ module ImageChipper
     export chip, chip_with_boxes
 
 end # module
-
-# Pkg.add([ "FileIO", "ImageMagick", "ImageView" ] )
-# using Images, FileIO, ImageMagick, ImageView
-# pic = load("/home/caseykneale/Downloads/train_images/10.tif");
-#
-#
-#
-# chippers = Chip( pic, ( 256, 256 ), (1.0,1.0) )
-#
-# chippers[:,:,1]
-# chippers[:,:,2]
-# chippers[:,:,3]
-# chippers[:,:,4]
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,37 @@
 using ImageChipper
 using Test
 
-@testset "ImageChipper.jl" begin
-    # Write your own tests here.
+@testset "Image Chipping" begin
+    faux_image = rand( 256, 256 )
+    chip_dims = ( 64, 64 )
+    chipped = chip( faux_image, chip_dims, ( 0.0, 0.0 ) )
+    #checkoutput dimensionality
+    @test all( size(chipped)[1:2] .== chip_dims )
+    @test size(chipped)[3] == 16
+    @test all( chipped[:,:,1] .== faux_image[ 1:chip_dims[1], 1:chip_dims[2] ] )
+    @test all( chipped[:,:,end] .== faux_image[   (end - chip_dims[1] + 1):end,
+                                        (end - chip_dims[2] + 1):end ] )
+    #checkoutput dimensionality
+    chipped = chip( faux_image, ( 60, 60 ), ( 0.0, 0.0 ) )
+    @test size(chipped)[3] .==  25
+    #checkoutput dimensionality
+    chipped = chip( faux_image, ( 32, 32 ), ( 0.0, 0.0 ) )
+    @test size(chipped)[3] .==  64
+end
+
+@testset "Image Chipping with Boxes" begin
+    using ObjectDetectionStats
+    faux_image = rand( 256, 256 )
+    chip_dims = ( 64, 64 )
+
+    boxes = [   Box(   1,   1,  22,  22 ),
+                Box(  55,  55, 222, 222 ),
+                Box( 222, 222, 264, 264 )   ]
+
+    chips, boxassignments = chip_with_boxes( faux_image, boxes, chip_dims, ( 0.0, 0.0 ), 0.0 )
+
+    @test( length(boxassignments[ 1 ]) == 2 )
+    @test( length(boxassignments[ 2 ]) == 1 )
+    @test( length(boxassignments[ 3 ]) == 1 )
+    @test( length(boxassignments[ 4 ]) == 2 )
 end
